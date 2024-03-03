187a2925c82541c1f9f34270abe6c478f90be6ba
jadercaro <jjadercaro192@gmail.com>
2024-03-03 18:39:37 -0500

    Back initial finish

--- vuelo/src/main/java/com/udea/vuelo/controller/FlightController.java
+++ vuelo/src/main/java/com/udea/vuelo/controller/FlightController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class FlightController {
 	@Autowired
 	private FlightService flightService;
 	
+	@GetMapping("/search")
 	public List<List<Flight>> searchFlights(
 			@RequestParam(name="startDate") String startDate, 
 			@RequestParam(name="endDate") String endDate
--- vuelo/src/main/java/com/udea/vuelo/service/FlightService.java
+++ vuelo/src/main/java/com/udea/vuelo/service/FlightService.java
@@ -3,8 +3,10 @@ package com.udea.vuelo.service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -15,7 +17,7 @@ import com.udea.vuelo.model.Flight;
 @Service
 public class FlightService {
 	//Ruta del archivo
-	private final String filepath ="classpath:flights.json";
+	private final String filepath ="classpath:flight.json";
 	
 	//Metodo de la logica de busqueda de vuelos
 	public List<List<Flight>> searchFlights(LocalDate startDate, LocalDate endDate){
@@ -36,6 +38,8 @@ public class FlightService {
 			throw new RuntimeException("Error leyendo el archivo Json",e);
 		}
 	}
+	
+	
 	private boolean isDatateInRange(LocalDate dateToCheck, LocalDate startDate, LocalDate endDate) {
 	//verifica si la fecha esta en el rango correcto
 	return !dateToCheck.isBefore(startDate) && !dateToCheck.isAfter(endDate);
